314e19c350cd5f65e7d7cbdc74e982d2954e43c1
Jeremy <jeremymiller00@gmail.com>
2018-11-01 16:38:35 -0700

    develop predict, report

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 import pickle
 import requests
+import sys
 
 # load in the model
 model = pickle.load(open('clf.p', 'wb'))
@@ -12,10 +13,27 @@ response = requests.post(url, json={'api_key': api_key,
                                     'sequence_number': sequence_number})
 raw_data = response.json()
 
-# vectorize input
+keep = ['event_created', 'event_end', 'event_published', 'event_start', 'fb_published','has_analytics', 'has_header', 'has_logo', 'object_id', 'org_facebook','org_twitter', 'show_map', 'user_age', 'user_created', 'user_type','venue_latitude' 'venue_longitude']
+
+def select_features(data_point):
+    '''
+    takes in a new data point and keeps only relevant features
+    '''
+    clean = {}
+    data_point = data_point['data'][0]
+    for key, value in data_point.items():
+        if key in keep:
+            clean.update({key: value})
+    return data_point
+
+# select relevant features
+data = select_features(raw_data)
+
+# one-hot categorical features
 
 
 # get prediction
-
+prediction = model.predict(data)
 
 # output prediction
+sys.stdout.write(prediction)
