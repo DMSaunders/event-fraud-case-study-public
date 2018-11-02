35fbb2166288d15db49a73ad599261e4a0e04105
DMSaunders <dmariesaunders@gmail.com>
2018-11-02 14:51:24 -0700

    server.py v3 w/ feature table functionality

--- server.py
+++ server.py
@@ -11,20 +11,26 @@ app = Flask(__name__)
 @app.route('/score', methods=['GET'])
 def import_DB():
     #THE QUERY
+    #feature_importances = feature_table.scan()      uncomment when feature table ready
     x = table.scan()
     return x
+    #return {feature_importances, x}     uncomment when feature table ready, not sure about format
+
 
 #needs to output
-# {causes:{1,2,3},
-# datapoint:{all records incl pred, label}
+# {causes:{1,2,3
+#   },
+# datapoint:{all records incl pred, label
+#   }
 # }
 
-#so far it only does the datapoints, and the causes are hardcoded
+#so far it only does the datapoints, and the causes are hardcoded  del this line when feature table ready
 
 if __name__ == '__main__':
     #connect to the dynamodb once
     dynamo = boto3.resource('dynamodb')
     table = dynamo.Table('fraud_detection')
+    #feature_table = dynamo.Table('feature_table')    uncomment when feature table ready
     
     #run the app
     app.run(host='0.0.0.0', port=8080, debug=True)
